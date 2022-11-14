b46a51081c06d48d0ea1a9be1bba3d172e9197a5
ApTeM1986 <saartem@gmail.com>
2022-11-14 17:48:08 +0100

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Artem Stetsenko on 14.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
